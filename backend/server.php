a4452dff8bd502ae91705bd158ccdfba322f850f
Lee Seng Cheong <sengcheong94@gmail.com>
2017-11-07 13:27:01 +0800

    Added carpark data in CSV & JSON format

--- backend/server.php
+++ backend/server.php
@@ -11,6 +11,41 @@
 	else
 	{
 		$server = new ServerCarparkCollection($dbhost, $dbuser, $dbname, $dbpass, $port);
+
+
+		$carpark_list = $server->getCollection();
+
+		$json_response = array();
+		foreach($carpark_list as $carpark)
+		{
+			// $json_response[$carpark->getId()] = array();
+			// $json_response[$carpark->getId()]
+		}
+
+
+		echo "<pre>";
+		print_r(json_encode($server->getCollection()));
+		echo "</pre>";
+
+
+		echo "<pre>";
+		echo print_r($server->getCollection());
+		echo "</pre>";
+
+		/*
+		if($_SERVER['REQUEST_METHOD'] == 'POST')
+		{
+			switch ($_POST['Action']) {
+				case 'GET_ALL':
+					# code...
+					break;
+				
+				default:
+					# code...
+					break;
+			}
+		}
+		*/
 	}
 
 ?>
