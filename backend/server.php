6c264cd68e80b792b8ced685b24a48e4a83ca9ca
Lee Seng Cheong <sengcheong94@gmail.com>
2017-11-07 22:26:37 +0800

    Server now returns json encoded data

--- backend/server.php
+++ backend/server.php
@@ -18,19 +18,57 @@
 		$json_response = array();
 		foreach($carpark_list as $carpark)
 		{
-			// $json_response[$carpark->getId()] = array();
-			// $json_response[$carpark->getId()]
+			$location_json = array();
+			$rates_json = array();
+			
+			$location = $carpark->getLocation();
+			$location_json['longitude'] = $location->getLongtitude();
+			$location_json['latitude'] = $location->getLatitude();
+			$car_json['GPS'] = $location_json;
+
+
+			$rate_dict = $carpark->getRates();
+
+			foreach ($rate_dict as $dayType => $rates) {
+
+				foreach($rates as $index => $rate)
+				{
+					$rate_json = array();
+					$rate_json['startTime'] = $rate->getStartTime();
+					$rate_json['endTime'] = $rate->getEndTime();
+					$rate_json['prices'] = explode(',',$rate->getPrices());
+
+					if(array_key_exists($dayType, $rates_json))
+					{
+						$temp = $rates_json[$dayType];
+						array_push($temp, $rate_json);
+						$rates_json[$dayType] = $temp;
+					}
+					else
+					{
+						$rates_json[$dayType] = array($rate_json);
+					}
+				}
+			}
+
+			$json_response[$carpark->getId()] = array(
+					"name" => $carpark->getName(),
+					"avaliableLots" => $carpark->getAvaliableLots(),
+					"Rates" => $rates_json,
+					"GPS" => $location_json
+				);
+
 		}
 
 
 		echo "<pre>";
-		print_r(json_encode($server->getCollection()));
+		echo print_r($json_response);
 		echo "</pre>";
 
 
-		echo "<pre>";
-		echo print_r($server->getCollection());
-		echo "</pre>";
+		// echo "<pre>";
+		// echo print_r($server->getCollection());
+		// echo "</pre>";
 
 		/*
 		if($_SERVER['REQUEST_METHOD'] == 'POST')
