6b7d372979b571f8de4870e7d349e64efe3973cc
frozonnorth <khiewbin@gmail.com>
2017-11-12 02:37:05 +0800

    matching 1 for 1 with class diagram

--- dist/js/ViewNearbyCarparkUI.js
+++ dist/js/ViewNearbyCarparkUI.js
@@ -4,10 +4,42 @@ myApp.onPageBeforeInit('home', function (page) {
     userController.get_lots();
     userController.initMap();
 });
-//View nearbymethod
-//Get Current Location of User and set map to current location
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
+
+function getSelectedCarpark() {
+    return this.selectedCarpark;
+};
+
+function setSelectedCarpark(selectedCarpark) {
+    this.selectedCarpark = selectedCarpark;
+};
+
+function selectCarpark()
+{
+    var response;
+
+    $.ajax({
+        url: "backend/viewNearbyCarparkUI.php",
+        method: "POST",
+        data: {'Action': 'GET_selectCarpark' },
+        dataType: 'json',
+        async: false,
+        success:function(data,response)
+        {
+            console.log(data);
+            selected_carpark = data;
+            reponse = data;
+        },
+        error:function(jqXHR, text, errorThrown)
+        {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(errorThrown);
+        }
+    });
+
+    return reponse;
+}
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -25,8 +57,153 @@ function getLocation() {
     }
 }
 
+function enterSearchtext() 
+{
+    var response;
 
-//Set Destination
-function setDestination(){
-    carparkPlan = true;
-}
+    $.ajax({
+        url: "backend/viewNearbyCarparkUI.php",
+        method: "POST",
+        data: {'Action': 'SET_searchtext' },
+        dataType: 'json',
+        async: false,
+        success:function(data,response)
+        {
+            console.log(data);
+            reponse = data;
+        },
+        error:function(jqXHR, text, errorThrown)
+        {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(errorThrown);
+        }
+    });
+
+    return reponse;
+};
+function displayFoundCarpark() {
+    var response;
+    var foundcarpark;
+
+    $.ajax({
+        url: "backend/viewNearbyCarparkUI.php",
+        method: "POST",
+        data: {'Action': 'GET_FoundCarpark' },
+        dataType: 'json',
+        async: false,
+        success:function(data,response)
+        {
+            console.log(data);
+            foundcarpark = data;
+            reponse = data;
+        },
+        error:function(jqXHR, text, errorThrown)
+        {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(errorThrown);
+        }
+    });
+
+    return reponse;
+};
+function planRoute() {
+    var response;
+
+    $.ajax({
+        url: "backend/viewNearbyCarparkUI.php",
+        method: "POST",
+        data: {'Action': 'SET_planRoute' },
+        dataType: 'json',
+        async: false,
+        success:function(data,response)
+        {
+            console.log(data);
+            reponse = data;
+        },
+        error:function(jqXHR, text, errorThrown)
+        {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(errorThrown);
+        }
+    });
+
+    return reponse;
+};
+function displayCurrentLocation() {
+    var response;
+
+    $.ajax({
+        url: "backend/viewNearbyCarparkUI.php",
+        method: "POST",
+        data: {'Action': 'SET_displayCurrentLocation' },
+        dataType: 'json',
+        async: false,
+        success:function(data,response)
+        {
+            console.log(data);
+            reponse = data;
+        },
+        error:function(jqXHR, text, errorThrown)
+        {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(errorThrown);
+        }
+    });
+
+    return reponse;
+};
+function viewNearbyCarpark(dest) {
+    var response;
+    var dest;
+
+    $.ajax({
+        url: "backend/viewNearbyCarparkUI.php",
+        method: "POST",
+        data: {'Action': 'GET_NearbyCarpark' ,'Dest':'GET_dest' },
+        dataType: 'json',
+        async: false,
+        success:function(data,response)
+        {
+            console.log(data);
+            dest = data;
+            reponse = data;
+        },
+        error:function(jqXHR, text, errorThrown)
+        {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(errorThrown);
+        }
+    });
+
+    return reponse;
+};
+
+function viewNearbyCarpark(radius) {
+    var response;
+
+    $.ajax({
+        url: "backend/viewNearbyCarparkUI.php",
+        method: "POST",
+        data: {'Action': 'GET_viewNearbyCarpark' ,'Radius':'GET_radius'},
+        dataType: 'json',
+        async: false,
+        success:function(data,response)
+        {
+            console.log(data);
+            reponse = data;
+        },
+        error:function(jqXHR, text, errorThrown)
+        {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(errorThrown);
+        }
+    });
+
+    return reponse;
+};
