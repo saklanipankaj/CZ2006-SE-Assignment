d07ca83a4e1403c197002330b6d0f7e807d4eefc
SaklaniPankaj <drag0n_freak@hotmail.com>
2017-11-12 02:17:03 +0800

    Update fixed

--- dist/js/RecordCarparkLotInformationUI.js
+++ dist/js/RecordCarparkLotInformationUI.js
@@ -4,8 +4,7 @@ myApp.onPageInit('recordCP', function (page) {
 });
 
 //For RecordCPInfo
-
-function calculateCost(elapsedTime){
+function calculateCost1(elapsedTime){
 
     var carpark = json[selectedCarpark];
     var now = moment();
@@ -215,6 +214,218 @@ function calculateCost(elapsedTime){
     document.getElementById('fee').innerHTML = "$"+total/100;
 }
 
+
+function calculateCost(elapsedTime){
+    document.getElementById('slideroutput').innerHTML=elapsedTime +"Hour";
+    var carpark = json[selectedCarpark];
+    var now = moment();
+    var end = moment().add(document.getElementById('slideroutput').innerHTML=elapsedTime, 'h');
+    var total = 0;
+    var count =0;
+    while(!now.isSameOrAfter(end)){
+        if(carpark['Rates']==null)
+            break;
+
+        if(count>2){
+            console.log("INFINTE LOOP/ CARPARK NO 24H Parking");
+            break;
+        }
+        count++;
+
+        if(now.day()!=0 && now.day()!=6){
+            if(carpark['Rates']['Weekday']!=null){
+                var weekday = carpark['Rates']['Weekday'];
+                for(rate in weekday){
+                    //used for 2 loops where day could have changed to weekends
+                    if(now.day()==0 && now.day()==6)
+                        break;
+
+                    var start = weekday[rate]['startHour'];
+                    var startTime = moment(now).hours(start);
+                    startTime.minutes((start*100)%100);
+                    var end2 = weekday[rate]['endHour'];
+                    var endTime = moment(now).hours(end2);
+                    endTime.minutes((end2*100)%100);
+                    if(endTime.isBefore(startTime)){
+                        endTime.add(1,'d');
+                        console.log("EndTime day Added!");
+                    }
+
+                    if(!now.isBetween(startTime,endTime,null,'[]'))
+                        continue;
+
+                    var prices = weekday[rate]['pricePerHalfHour'];
+                    if(prices[1]==0){
+                        if(total==0)
+                            total+= prices[0]*100;
+
+                        if(endTime.isBefore(end))
+                            now = endTime;
+                        else
+                            now = end;
+
+                        now.add(1,'m');
+                    }
+                    else{
+                        //1St Hour and subsequent Hour
+                        if(prices.length>2){
+                            var i, last;
+                            for(i=0;i<prices.length;i++){
+                                console.log('First Loop');
+                                total+=(prices[i]*100);
+                                now.add(30,'m');
+                                last=prices[i]*100;
+                                if(!now.isBetween(startTime,endTime,null,'[]')||now.isSameOrAfter(end))
+                                    break;
+                            }
+                            while(now.isBetween(startTime,endTime,null,'[]')&&!now.isSameOrAfter(end)){
+                                console.log('Subsequent Loop');
+                                total+=last;
+                                now.add(30,'m');
+                            }
+                            console.log("Subsequent Loop Exited");
+                        }
+                        else{
+                            while(now.isBetween(startTime,endTime,null,'[]')&&!now.isSameOrAfter(end)){
+                                total+=prices[0]*100;
+                                now.add(30,'m');
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        else if(now.day()==6){
+            if(carpark['Rates']['Saturday']!=null){
+                var saturday = carpark['Rates']['Saturday'];
+                for(rate in saturday){
+                    //used for 2 loops where day could have changed to weekends
+                    if(now.day()!=6)
+                        break;
+
+                    var start = saturday[rate]['startHour'];
+                    var startTime = moment(now).hours(start);
+                    startTime.minutes((start*100)%100);
+                    var end2 = saturday[rate]['endHour'];
+                    var endTime = moment(now).hours(end2);
+                    endTime.minutes((end2*100)%100);
+
+                    if(endTime.isBefore(startTime)){
+                        endTime.add(1,'d');
+                        console.log("EndTime day Added!");
+                    }
+
+                    if(!now.isBetween(startTime,endTime,null,'[]'))
+                        continue;
+                    var prices = saturday[rate]['pricePerHalfHour'];
+                    if(prices[1]==0){
+                        if(total==0)
+                            total+= prices[0]*100;
+
+                        if(endTime.isBefore(end))
+                            now = endTime;
+                        else
+                            now = end;
+
+                        now.add(1,'m');
+                    }
+                    else{
+                        //1St Hour and subsequent Hour
+                        if(prices.length>2){
+                            var i, last;
+                            for(i=0;i<prices.length;i++){
+                                total+=(prices[i]*100);
+                                now.add(30,'m');
+                                last=prices[i]*100;
+                                if(!now.isBetween(startTime,endTime,null,'[]')||now.isSameOrAfter(end))
+                                    break;
+                            }
+                            while(now.isBetween(startTime,endTime,null,'[]')&&!now.isSameOrAfter(end)){
+                                total+=last;
+                                now.add(30,'m');
+                            }
+                        }
+                        else{
+                            while(now.isBetween(startTime,endTime,null,'[]')&&!now.isSameOrAfter(end)){
+                                total+=prices[0]*100;
+                                now.add(30,'m');
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+        else{
+            if(carpark['Rates']['Sunday']!=null){
+                var sunday = carpark['Rates']['Sunday'];
+                for(rate in sunday){
+                    if(now.day()!=0)
+                        break;
+
+                    var start = sunday[rate]['startHour'];
+                    var startTime = moment().hours(start);
+                    startTime.minutes((start*100)%100);
+                    var end2 = sunday[rate]['endHour'];
+                    var endTime = moment().hours(end2);
+
+                    if(now.isBefore(start)&&rate==0)
+                        now.add(-1,'d');
+
+                    if(endTime.isBefore(startTime)){
+                        endTime.add(1,'d');
+                        console.log("EndTime day Added!");
+                    }
+                    if(!now.isBetween(startTime,endTime,null,'[]'))
+                        continue;
+
+                    var prices = sunday[rate]['pricePerHalfHour'];
+                    //per entry display
+                    if(prices[1]==0){
+                        if(total==0)
+                            total+= prices[0]*100;
+
+                        if(endTime.isBefore(end))
+                            now = endTime;
+                        else
+                            now = end;
+
+                        now.add(1,'m');
+                    }
+                    else{
+                        //1St Hour and subsequent Hour
+                        if(prices.length>2){
+                            var i, last;
+                            for(i=0;i<prices.length;i++){
+                                total+=(prices[i]*100);
+                                now.add(30,'m');
+                                last=prices[i]*100;
+                                if(!now.isBetween(startTime,endTime,null,'[]')||now.isSameOrAfter(end))
+                                    break;
+                            }
+                            while(now.isBetween(startTime,endTime,null,'[]')&&!now.isSameOrAfter(end)){
+                                total+=last;
+                                now.add(30,'m');
+                            }
+                        }
+                        else{
+                            while(now.isBetween(startTime,endTime,null,'[]')&&!now.isSameOrAfter(end)){
+                                total+=prices[0]*100;
+                                now.add(30,'m');
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    document.getElementById('calculatefee').innerHTML = "$"+total/100;
+}
+
+
+
+
 function resetTimer()
 {
     // Set the date we're counting down to
@@ -238,10 +449,10 @@ function resetTimer()
 
         // change the price
         if(seconds==1&&hours==0&&minutes==0)
-            calculateCost(1);
+            calculateCost1(1);
 
         if(hours>1&&minutes==0&&seconds==0)
-            calculateCost(minutes);
+            calculateCost1(minutes);
 
     }, 1000);
 }
